f060279def409ca4d304de061b329fd7ac58fb13
Nicholas <nicholaskohlbecker@gmail.com>
2024-06-30 16:39:56 -0400

    A Java Program that calls to find How many sugar lumps its to get a building to specfic value

---
+++ SugarLumpLevel.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class SugarLumpLevel 
+{ 
+
+   private HashMap<Integer,Integer> calculatedLumps = new HashMap<>();
+   public static void main(String[] args)
+   {
+      System.out.println("Enter the Level you would like to get the building to");
+      Scanner scnr = new Scanner(System.in);
+      int BulidLevel = scnr.nextInt();
+      System.out.println(SugarLumps(BulidLevel, new HashMap<>()));
+   }
+   public static int SugarLumps(int n, HashMap<Integer,Integer> calculatedLumps)
+   {
+      if(n == 1)
+      {
+         return 1; 
+      }
+      if(n == 2)
+      {
+         return 3;
+      }
+      if(calculatedLumps.containsKey(n))
+      {
+         return calculatedLumps.get(n);
+      }
+      
+      int results = n + SugarLumps(n - 1,calculatedLumps);
+      calculatedLumps.put(n,results);
+      return results;
+     
+   }
+}
